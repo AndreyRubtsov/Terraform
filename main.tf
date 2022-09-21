01efd0a43517be4c780c421166d7eb3e05d80214
Andrey Rubtsov <mail@undrey.com>
2022-09-22 00:36:16 +0300

    Working on adding ECS

--- main.tf
+++ main.tf
@@ -230,10 +230,10 @@ resource "aws_security_group" "ec2_pool" {
 
 resource "aws_security_group_rule" "ec2_pool_rule" {
   from_port                = 2368
+  to_port                  = 2368
   protocol                 = "tcp"
   security_group_id        = aws_security_group.ec2_pool.id
   source_security_group_id = aws_security_group.alb.id
-  to_port                  = 2368
   type                     = "ingress"
 }
 
